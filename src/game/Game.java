df9757922c96d099367cf0d334b574f38e69a01d
Philipp Rehs <P.Rehs@gmx.net>
2012-05-08 19:38:02 +0200

    fixed errors readded comments

--- src/game/Game.java
+++ src/game/Game.java
@@ -13,17 +13,16 @@ import level.Box;
 import entities.Ball;
 import entities.BreakeableWall;
 import entities.Entity;
-import entities.background;
 
 public class Game extends Canvas implements Runnable {
 
 	public static final int BLOCK_SIZE = 50;
 
 	public static int FIELD_WIDTH = 15;
+	public static int FIELD_HEIGHT = ((Game.FIELD_WIDTH * 3) / 4);
 
 	public static final int GAME_WIDTH = Game.FIELD_WIDTH * Game.BLOCK_SIZE;
-	public static final int GAME_HEIGHT = ((Game.FIELD_WIDTH * 3) / 4)
-			* Game.BLOCK_SIZE;
+	public static final int GAME_HEIGHT = Game.FIELD_HEIGHT * Game.BLOCK_SIZE;
 
 	public static final int SCALE = 1;
 	private static ArrayList<Entity> entities = new ArrayList<Entity>();
@@ -37,10 +36,13 @@ public class Game extends Canvas implements Runnable {
 
 	private InputHandler keys;
 
+	/**
+	 * Constructor to set Canvas size and create important objects and add some
+	 * Test objects
+	 */
 	public Game() {
 		Debug.setMode(Debug.DEBUG);
-		Dimension d = new Dimension(Game.GAME_WIDTH * Game.SCALE,
-				Game.GAME_HEIGHT * Game.SCALE);
+		Dimension d = new Dimension(Game.GAME_WIDTH * Game.SCALE, Game.GAME_HEIGHT * Game.SCALE);
 		this.setPreferredSize(d);
 		this.setMinimumSize(d);
 		this.setMaximumSize(d);
@@ -48,17 +50,15 @@ public class Game extends Canvas implements Runnable {
 		this.keys = new InputHandler();
 		this.addKeyListener(this.keys);
 		// draw background
-		for (int i = 0; i < (Game.FIELD_WIDTH * 40); i = i + Game.BLOCK_SIZE) {
-			for (int j = 0; j < (Game.FIELD_WIDTH * 50); j = j
-					+ Game.BLOCK_SIZE) {
-				Game.entities.add(new background(j, i));
+		for (int i = 0; i < (Game.GAME_WIDTH); i = i + Game.BLOCK_SIZE) {
+			for (int j = 0; j < (Game.GAME_HEIGHT); j = j + Game.BLOCK_SIZE) {
+				// Game.entities.add(new background(j, i));
 			}
 
 		}
 
 		for (int x = 0; x < Game.FIELD_WIDTH; x++) {
-			Game.entities.add(new BreakeableWall(x * Game.BLOCK_SIZE, x
-					* Game.BLOCK_SIZE));
+			Game.entities.add(new BreakeableWall(x * Game.BLOCK_SIZE, x * Game.BLOCK_SIZE));
 
 		}
 
@@ -67,6 +67,12 @@ public class Game extends Canvas implements Runnable {
 		}
 	}
 
+	/**
+	 * Game-Loop Check how long it took to render a frame and let the thread
+	 * sleep some ns
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
 	@Override
 	public void run() {
 		long lastLoopTime = System.nanoTime();
@@ -87,14 +93,23 @@ public class Game extends Canvas implements Runnable {
 				this.fps = 0;
 			}
 
+			/**
+			 * Move all objects
+			 */
 			this.step(delta);
 
+			/**
+			 * Redraw all objects
+			 */
 			BufferStrategy bs = this.getBufferStrategy();
 			Graphics g = bs.getDrawGraphics();
 			this.draw(g);
 			bs.show();
 			Toolkit.getDefaultToolkit().sync();
 
+			/**
+			 * Let the thread sleep
+			 */
 			sleepTime = (lastLoopTime - System.nanoTime()) / this.frameTimeNs;
 			if (sleepTime < this.minSleepTime) {
 				sleepTime = this.minSleepTime;
@@ -108,6 +123,9 @@ public class Game extends Canvas implements Runnable {
 		}
 	}
 
+	/**
+	 * Gets called form Launcher to start the game;
+	 */
 	public void start() {
 		this.running = true;
 		this.createBufferStrategy(2);
@@ -116,10 +134,18 @@ public class Game extends Canvas implements Runnable {
 		this.requestFocus();
 	}
 
+	/**
+	 * Stop the game
+	 */
 	public void stop() {
 		this.running = false;
 	}
 
+	/**
+	 * Move all entities
+	 * 
+	 * @param delta
+	 */
 	private void step(double delta) {
 
 		for (Entity e : Game.entities) {
@@ -129,10 +155,14 @@ public class Game extends Canvas implements Runnable {
 		}
 	}
 
+	/**
+	 * Draw everything
+	 * 
+	 * @param g
+	 */
 	private void draw(Graphics g) {
 		g.setColor(this.getBackground());
-		g.fillRect(0, 0, (Game.GAME_WIDTH * Game.SCALE) + 10,
-				(Game.GAME_HEIGHT * Game.SCALE) + 10);
+		g.fillRect(0, 0, (Game.GAME_WIDTH * Game.SCALE) + 10, (Game.GAME_HEIGHT * Game.SCALE) + 10);
 		for (Entity e : Game.entities) {
 			if (e.removed == false) {
 				e.draw(g);
@@ -142,10 +172,18 @@ public class Game extends Canvas implements Runnable {
 		g.drawString("FPS: " + this.fps_static, 0, 10);
 	}
 
+	/**
+	 * Get all Entities in a Box
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return List<Entity> Found entities
+	 */
 	public static List<Entity> getEntities(int x1, int y1, int x2, int y2) {
 		List<Entity> result = new ArrayList<Entity>();
-		Box b = new Box(Math.max(0, x1), Math.max(0, y1), Math.min(x2,
-				Game.GAME_WIDTH), Math.min(y2, Game.GAME_HEIGHT));
+		Box b = new Box(Math.max(0, x1), Math.max(0, y1), Math.min(x2, Game.GAME_WIDTH), Math.min(y2, Game.GAME_HEIGHT));
 
 		for (Entity e : Game.entities) {
 			if (e.removed == false) {
