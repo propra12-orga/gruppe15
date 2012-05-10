a89ea9a2f62b377c5a651b2b59d1131ae5efa3c7
Arsham Sabbaghi Asl <Arsham.Sabbaghi.Asl@uni-duesseldorf.de>
2012-05-10 16:04:17 +0200

    Fixed the Level-Loader

--- src/level/Loader.java
+++ src/level/Loader.java
@@ -10,17 +10,17 @@ public class Loader {
 
 		int i, j, l, k = 0;
 		int[][] arr = new int[zeilen][spalten];
-		for (i = 0; i < spalten; i++) {
-			for (j = 0; j < zeilen; j++) {
-				arr[j][i] = Integer.parseInt("" + S.charAt(k));
-				l = arr[j][i];
+		for (i = 0; i < zeilen; i++) {
+			for (j = 0; j < spalten; j++) {
+				arr[i][j] = Integer.parseInt("" + S.charAt(k));
+				l = arr[i][j];
 
 				if (l == 1) {
-					Game.entities.add(new BreakableWall(i * Game.BLOCK_SIZE, j
+					Game.entities.add(new BreakableWall(j * Game.BLOCK_SIZE, i
 							* Game.BLOCK_SIZE));
 				}
 				if (l == 2) {
-					Game.entities.add(new Wall(i * Game.BLOCK_SIZE, j
+					Game.entities.add(new Wall(j * Game.BLOCK_SIZE, i
 							* Game.BLOCK_SIZE));
 				}
 				k++;
