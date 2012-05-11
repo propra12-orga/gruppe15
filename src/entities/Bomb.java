4c3bd2d0169630a828b2653f00e20f6a7e1b32fc
Oktay Sarier <Garet12@googlemail.com>
2012-05-11 08:08:35 +0200

    small changes

--- src/entities/Bomb.java
+++ src/entities/Bomb.java
@@ -1,14 +1,38 @@
 package entities;
 
+import game.Game;
 import graphics.Sprite;
 
+import java.awt.Graphics;
+
+import level.Box;
+
 public class Bomb extends Entity {
-	public Bomb(int x, int y) {
+	private boolean activation;
+
+	public Bomb(int x, int y, boolean activation) {
 		super(x, y);
+		this.activation = activation;
 		this.images = Sprite.load("bomb_small.png", 100, 100);
+		this.box = new Box(this.x, this.y, this.x * 2, this.y * 2);
 	}
 
 	public void explode() {
+		int timer;
+		timer = 5;
+		while (this.activation) {
+			for (int i = 0; i < timer; i++) {
 
+			}
+
+			this.activation = false;
+		}
+
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage((this.images[0][0]).image, this.x, this.y, Game.BLOCK_SIZE,
+				Game.BLOCK_SIZE, null);
 	}
 }
