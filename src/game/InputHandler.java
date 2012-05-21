1e6052c66706a688cf0879d2cc3e72b2c278432a
Philipp Rehs <P.Rehs@gmx.net>
2012-05-21 15:05:32 +0200

    debug removed

--- src/game/InputHandler.java
+++ src/game/InputHandler.java
@@ -29,7 +29,6 @@ public class InputHandler implements KeyListener {
 	}
 
 	private void toggle(KeyEvent ke, boolean pressed) {
-		Debug.log(Debug.DEBUG, "key pressed");
 		if (ke.getKeyCode() == KeyEvent.VK_UP) {
 			this.up.toggle(pressed);
 		}
