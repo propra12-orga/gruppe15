6a91dfa2388d91c7106983f92ec6686f6b386b13
Arsham Sabbaghi Asl <Arsham.Sabbaghi.Asl@uni-duesseldorf.de>
2012-06-13 14:33:43 +0200

    Fixed randomness of breakable walls

--- src/entities/BreakableWall.java
+++ src/entities/BreakableWall.java
@@ -13,11 +13,11 @@ public class BreakableWall extends Wall {
 	 */
 	public BreakableWall(int x, int y) {
 		super(x, y);
-		int z = new Random().nextInt(10);
+		int z = new Random().nextInt(12);
 		// int z = (int) (Math.random() * 10);
 		if (z < 4) {
 			this.images = Sprite.load("w1.png", 100, 100);
-		} else if ((z >= 3) && (z < 8)) {
+		} else if ((z >= 4) && (z < 8)) {
 			this.images = Sprite.load("w2.png", 100, 100);
 		} else if (z >= 8) {
 			this.images = Sprite.load("w3.png", 100, 100);
