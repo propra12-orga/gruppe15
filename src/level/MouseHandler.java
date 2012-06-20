5adfd50950e9916499169eaf29b3c1ade1008c93
Kyra Mooren <kyra.mooren@uni-duesseldorf.de>
2012-06-20 16:05:40 +0200

    created MouseHandler

---
+++ src/level/MouseHandler.java
@@ -0,0 +1,38 @@
+package level;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseHandler implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		// setComponent(arg0.getX(), arg0.getY());
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
