18de2880333c9ca39ce1e708e042f56c382fbbdc
Oktay Sarier <Garet12@googlemail.com>
2012-06-26 12:35:51 +0200

    Sound works in Linux again

--- src/game/Sounds.java
+++ src/game/Sounds.java
@@ -1,8 +1,10 @@
 package game;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
 public class Sounds {
 	public Sounds(String filename) {
@@ -16,10 +18,18 @@ public class Sounds {
 
 		Clip clip;
 		try {
-			clip = AudioSystem.getClip();
-			AudioInputStream inputStream = AudioSystem.getAudioInputStream(Main.class
-					.getResourceAsStream("/ressources/sound/" + this.filename));
-			clip.open(inputStream);
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(Main.class
+							.getResource("/ressources/sound/" + this.filename));
+			AudioFormat af = audioInputStream.getFormat();
+			int size = (int) (af.getFrameSize() * audioInputStream
+					.getFrameLength());
+			byte[] audio = new byte[size];
+			DataLine.Info info = new DataLine.Info(Clip.class, af, size);
+			audioInputStream.read(audio, 0, size);
+
+			clip = (Clip) AudioSystem.getLine(info);
+			clip.open(af, audio, 0, size);
 			clip.start();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
