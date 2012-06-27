a05e506c7cffdb562bb3ba5ca3b19d97bb3bbbde
MAurice Schleussinger <Maurice.Schleussinger@uni-duesseldorf.de>
2012-06-27 22:50:50 +0200

    fixed generator (working!)

--- src/level/Generator.java
+++ src/level/Generator.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Generator {
-
-	// Loader l1 = new Loader();
 	FileWriter fw;
 	File file;
 	int fieldheight;
@@ -18,13 +16,12 @@ public class Generator {
 		this.fieldheight = fieldheight;
 		this.fieldwith = fieldwith;
 		this.maparray = new char[this.fieldheight][this.fieldwith];
-
 		this.file = new File("src/ressources/maps/genMap");
 		try {
 			this.fw = new FileWriter(this.file);
 
+			// create map-frame
 			for (int j = 0; j < this.fieldheight; j++) {
-
 				this.maparray[j][0] = '2';
 				if ((j >= (this.fieldheight - 1)) || (j == 0)) {
 					for (int i = 1; i < (this.fieldwith - 1); i++) {
@@ -32,61 +29,67 @@ public class Generator {
 					}
 				} else {
 					for (int i = 1; i < (this.fieldwith - 1); i++) {
-
-						this.maparray[j][i] = '0';
+						this.maparray[j][i] = '1';
 					}
 				}
-
 				this.maparray[j][this.fieldwith - 1] = '2';
 			}
-			// unbegehbaren Teil erstellen
-			for (int j = 1; j < (this.fieldheight - 1); j++) {
-				for (int i = 1; i < (this.fieldwith - 1); i++) {
-					if (new Random().nextBoolean() == true) {
-						this.maparray[j][i] = '2';
+			// create indestructible blocks
+			for (int j = 1; j < (this.fieldheight - 2); j += 3) {
+				for (int i = 1; i < (this.fieldwith - 2); i += 3) {
+					int rnd = new Random().nextInt(13);
+					if (rnd <= 1) {
+						// single block
+						this.maparray[j + rnd + 1][i + rnd + 1] = '2';
+					} else if (rnd <= 3) {
+						// L
+						this.maparray[j + 1][i + 1] = '2';
+						this.maparray[j + 2][i + 1] = '2';
+						this.maparray[j + 2][i + 2] = '2';
+					} else if (rnd <= 5) {
+						// L
+						this.maparray[j + 1][i + 2] = '2';
+						this.maparray[j + 1][i + 1] = '2';
+						this.maparray[j + 2][i + 2] = '2';
+					} else if (rnd <= 7) {
+						// diagonal
+						this.maparray[j + 1][i + 1] = '2';
+						this.maparray[j + 2][i + 2] = '2';
+					} else if (rnd <= 9) {
+						this.maparray[j + 1][i + 2] = '2';
+						this.maparray[j + 2][i + 1] = '2';
 					} else {
-						this.maparray[j][i] = '0';
+						// square
+						this.maparray[j + 1][i + 1] = '2';
+						this.maparray[j + 1][i + 2] = '2';
+						this.maparray[j + 2][i + 1] = '2';
+						this.maparray[j + 2][i + 2] = '2';
 					}
 				}
 			}
-			// Player spawnen
+			// create the players
 			int rnd = new Random().nextInt((this.fieldheight - 3)) + 1;
-			this.maparray[1][rnd] = '3';
-			this.maparray[1][rnd + 1] = '0';
-			this.maparray[2][rnd] = '0';
-			this.maparray[this.fieldheight - 2][rnd] = '3';
-			this.maparray[this.fieldheight - 2][rnd + 1] = '0';
-			this.maparray[this.fieldheight - 3][rnd] = '0';
-			// Ziel spawnen
-			this.maparray[this.fieldheight / 2][this.fieldwith / 2] = '5';
-
-			// Generiertes ueberpruefen
-			for (int j = 1; j < (this.fieldheight - 1); j++) {
-				for (int i = 1; i < (this.fieldwith - 1); i++) {
-					if (this.maparray[j][i] == '3') {
-						if (this.maparray[j + 1][i] == '0') {
-							this.maparray[j + 1][i] = '1';
-						} else if (this.maparray[j + -1][i] == '0') {
-							this.maparray[j + -1][i] = '1';
-						}
-					}
-
-				}
-			}
-
-			// generiertes Feld schreiben
+			this.maparray[1][rnd - 1] = '3';
+			this.maparray[this.fieldheight - 2][rnd - 1] = '3';
+			// create some space around the players
+			this.maparray[1][rnd] = '0';
+			this.maparray[2][rnd - 1] = '0';
+			this.maparray[this.fieldheight - 2][rnd] = '0';
+			this.maparray[this.fieldheight - 3][rnd - 1] = '0';
+			// create a goal
+			int rnd2 = new Random().nextInt(this.fieldwith - 3) + 1;
+			this.maparray[this.fieldheight / 2][rnd2] = '5';
+			// write the generated map
 			for (int i = 0; i < (this.fieldheight); i++) {
 				for (int j = 0; j < (this.fieldwith); j++) {
 					this.fw.write(this.maparray[i][j]);
 				}
 				if (!(i >= (this.fieldheight - 1))) {
-
 					this.fw.write(System.getProperty("line.separator"));
 				}
 			}
 			this.fw.flush();
 		} catch (IOException e) {
-
 			e.printStackTrace();
 		}
 	}
