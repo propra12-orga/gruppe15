eb93da1f2f3426b726a32c42c924750cdd6679db
Philipp Rehs <P.Rehs@gmx.net>
2012-06-27 14:46:09 +0200

    Schuh grafik

--- src/entities/items/Schuh.java
+++ src/entities/items/Schuh.java
@@ -1,11 +1,16 @@
 package entities.items;
 
 import entities.Player;
+import game.Game;
+import graphics.Sprite;
+
+import java.awt.Graphics;
 
 public class Schuh extends Item {
 
 	public Schuh(int x, int y) {
 		super(x, y);
+		this.images = Sprite.load("items/speed.png", 48, 48);
 	}
 
 	@Override
@@ -18,4 +23,14 @@ public class Schuh extends Item {
 		p.speed /= 1.2;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see entities.Entity#draw(java.awt.Graphics)
+	 */
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage((this.images[0][0]).image, this.x, this.y, Game.BLOCK_SIZE,
+				Game.BLOCK_SIZE, null);
+	}
 }
