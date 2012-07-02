99cf0ac12a827fdcd75af09ec27f918726243393
Philipp Rehs <P.Rehs@gmx.net>
2012-07-02 18:29:37 +0200

    fixed handling of maps generating maps should work in applets

--- src/level/Generator.java
+++ src/level/Generator.java
@@ -6,7 +6,6 @@ package level;
  */
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Random;
 
 public class Generator {
@@ -36,85 +35,81 @@ public class Generator {
 	 * 
 	 * @param fieldheight
 	 * @param fieldwith
+	 * @return
 	 */
-	public void generateMap(int fieldheight, int fieldwith) {
+	public String generateMap(int fieldheight, int fieldwith) {
 		this.fieldheight = fieldheight;
 		this.fieldwith = fieldwith;
 		this.maparray = new char[this.fieldheight][this.fieldwith];
-		this.file = new File("src/ressources/maps/genMap");
-		try {
-			this.fw = new FileWriter(this.file);
-			// create map-frame
-			for (int j = 0; j < this.fieldheight; j++) {
-				this.maparray[j][0] = '2';
-				if ((j >= (this.fieldheight - 1)) || (j == 0)) {
-					for (int i = 1; i < (this.fieldwith - 1); i++) {
-						this.maparray[j][i] = '2';
-					}
-				} else {
-					for (int i = 1; i < (this.fieldwith - 1); i++) {
-						this.maparray[j][i] = '1';
-					}
+		StringBuilder map = new StringBuilder();
+		// create map-frame
+		for (int j = 0; j < this.fieldheight; j++) {
+			this.maparray[j][0] = '2';
+			if ((j >= (this.fieldheight - 1)) || (j == 0)) {
+				for (int i = 1; i < (this.fieldwith - 1); i++) {
+					this.maparray[j][i] = '2';
 				}
-				this.maparray[j][this.fieldwith - 1] = '2';
-			}
-			// create indestructible blocks
-			for (int j = 1; j < (this.fieldheight - 2); j += 3) {
-				for (int i = 1; i < (this.fieldwith - 2); i += 3) {
-					int rnd = new Random().nextInt(13);
-					if (rnd <= 1) {
-						// single block
-						this.maparray[j + rnd + 1][i + rnd + 1] = '2';
-					} else if (rnd <= 3) {
-						// L
-						this.maparray[j + 1][i + 1] = '2';
-						this.maparray[j + 2][i + 1] = '2';
-						this.maparray[j + 2][i + 2] = '2';
-					} else if (rnd <= 5) {
-						// L
-						this.maparray[j + 1][i + 2] = '2';
-						this.maparray[j + 1][i + 1] = '2';
-						this.maparray[j + 2][i + 2] = '2';
-					} else if (rnd <= 7) {
-						// diagonal
-						this.maparray[j + 1][i + 1] = '2';
-						this.maparray[j + 2][i + 2] = '2';
-					} else if (rnd <= 9) {
-						this.maparray[j + 1][i + 2] = '2';
-						this.maparray[j + 2][i + 1] = '2';
-					} else {
-						// square
-						this.maparray[j + 1][i + 1] = '2';
-						this.maparray[j + 1][i + 2] = '2';
-						this.maparray[j + 2][i + 1] = '2';
-						this.maparray[j + 2][i + 2] = '2';
-					}
+			} else {
+				for (int i = 1; i < (this.fieldwith - 1); i++) {
+					this.maparray[j][i] = '1';
 				}
 			}
-			// create the players
-			int rnd = new Random().nextInt((this.fieldheight - 4)) + 2;
-			this.maparray[1][rnd - 1] = '3';
-			this.maparray[this.fieldheight - 2][rnd - 1] = '3';
-			// create some space around the players
-			this.maparray[1][rnd] = '0';
-			this.maparray[2][rnd - 1] = '0';
-			this.maparray[this.fieldheight - 2][rnd] = '0';
-			this.maparray[this.fieldheight - 3][rnd - 1] = '0';
-			// create a goal
-			int rnd2 = new Random().nextInt(this.fieldwith - 4) + 2;
-			this.maparray[this.fieldheight / 2][rnd2] = '5';
-			// write the generated map
-			for (int i = 0; i < (this.fieldheight); i++) {
-				for (int j = 0; j < (this.fieldwith); j++) {
-					this.fw.write(this.maparray[i][j]);
-				}
-				if (!(i >= (this.fieldheight - 1))) {
-					this.fw.write(System.getProperty("line.separator"));
-				}
-			}
-			this.fw.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
+			this.maparray[j][this.fieldwith - 1] = '2';
 		}
+		// create indestructible blocks
+		for (int j = 1; j < (this.fieldheight - 2); j += 3) {
+			for (int i = 1; i < (this.fieldwith - 2); i += 3) {
+				int rnd = new Random().nextInt(13);
+				if (rnd <= 1) {
+					// single block
+					this.maparray[j + rnd + 1][i + rnd + 1] = '2';
+				} else if (rnd <= 3) {
+					// L
+					this.maparray[j + 1][i + 1] = '2';
+					this.maparray[j + 2][i + 1] = '2';
+					this.maparray[j + 2][i + 2] = '2';
+				} else if (rnd <= 5) {
+					// L
+					this.maparray[j + 1][i + 2] = '2';
+					this.maparray[j + 1][i + 1] = '2';
+					this.maparray[j + 2][i + 2] = '2';
+				} else if (rnd <= 7) {
+					// diagonal
+					this.maparray[j + 1][i + 1] = '2';
+					this.maparray[j + 2][i + 2] = '2';
+				} else if (rnd <= 9) {
+					this.maparray[j + 1][i + 2] = '2';
+					this.maparray[j + 2][i + 1] = '2';
+				} else {
+					// square
+					this.maparray[j + 1][i + 1] = '2';
+					this.maparray[j + 1][i + 2] = '2';
+					this.maparray[j + 2][i + 1] = '2';
+					this.maparray[j + 2][i + 2] = '2';
+				}
+			}
+		}
+		// create the players
+		int rnd = new Random().nextInt((this.fieldheight - 4)) + 2;
+		this.maparray[1][rnd - 1] = '3';
+		this.maparray[this.fieldheight - 2][rnd - 1] = '3';
+		// create some space around the players
+		this.maparray[1][rnd] = '0';
+		this.maparray[2][rnd - 1] = '0';
+		this.maparray[this.fieldheight - 2][rnd] = '0';
+		this.maparray[this.fieldheight - 3][rnd - 1] = '0';
+		// create a goal
+		int rnd2 = new Random().nextInt(this.fieldwith - 4) + 2;
+		this.maparray[this.fieldheight / 2][rnd2] = '5';
+		// write the generated map
+		for (int i = 0; i < (this.fieldheight); i++) {
+			for (int j = 0; j < (this.fieldwith); j++) {
+				map.append(this.maparray[i][j]);
+			}
+			if (!(i >= (this.fieldheight - 1))) {
+				map.append(System.getProperty("line.separator"));
+			}
+		}
+		return map.toString();
 	}
 }
--- src/level/Loader.java
+++ src/level/Loader.java
@@ -7,10 +7,19 @@ import entities.Player;
 import entities.Wall;
 import entities.WallWithFinishingPoint;
 import enums.Gamemode;
+import game.Debug;
 import game.Game;
 import game.Main;
 import input.KeySettings;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,65 +32,75 @@ public class Loader {
 	 * 
 	 * @param filename
 	 */
-	public void loadMap(String filename) {
+	public String loadMap(String filename) {
 
-		int x = 0, type, y = 0;
+		String content = "";
+
+		URL file = Main.class.getResource("/ressources/maps/" + filename);
+		if (file == null) {
+			Debug.log(Debug.ERROR, "Can't find map");
+		} else {
+			try {
+				content = Loader.readFile(file.getPath());
+			} catch (Exception e) {
+				try {
+					content = Loader.readFile(Main.class.getResourceAsStream("/ressources/maps/" + filename));
+				} catch (IOException e1) {
+					Debug.log(Debug.ERROR, "Can't read map");
+				}
+
+			}
+		}
+
+		return content;
+
+	}
+
+	public void loadMapFromString(String map_content) {
+		int x = 0, y = 0;
+		int i;
 		int player_count = 0;
-
-		Scanner maps;
-		try {
-
-			maps = new Scanner(
-					Main.class.getResourceAsStream("/ressources/maps/"
-							+ filename));
-			while (maps.hasNextLine()) {
-				String text = maps.nextLine();
-
-				for (x = 0; x < text.length(); x++) {
-					type = Integer.parseInt("" + text.charAt(x));
-					if (type == 0) {
-						Game.staticBackground.add(new Background(x
-								* Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
-					} else if (type == 1) {
-						Game.entities.add(new BreakableWall(
-								x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
-					} else if (type == 2) {
-						Game.entities.add(new Wall(x * Game.BLOCK_SIZE, y
-								* Game.BLOCK_SIZE));
-					} else if (type == 3) {
-						Player p = new Player(x * Game.BLOCK_SIZE, y
-								* Game.BLOCK_SIZE);
-						if (Game.gamemode != Gamemode.NETWORK) {
-							KeySettings keys = Game
-									.getKeySettings(player_count);
+		Scanner s = new Scanner(map_content);
+		while (s.hasNextLine()) {
+			String zeile = s.nextLine();
+			x = 0;
+			for (i = 0; i < zeile.length(); i++) {
+				int type = Integer.parseInt(String.valueOf(zeile.charAt(i)));
+				if (type == 0) {
+					Game.staticBackground.add(new Background(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
+				} else if (type == 1) {
+					Game.entities.add(new BreakableWall(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
+				} else if (type == 2) {
+					Game.entities.add(new Wall(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
+				} else if (type == 3) {
+					Player p = new Player(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE);
+					if (Game.gamemode != Gamemode.NETWORK) {
+						KeySettings keys;
+						try {
+							keys = Game.getKeySettings(player_count);
 							p.setKeys(keys);
 							player_count++;
 							Game.entities.add(p);
 							Game.players.add(p);
+						} catch (Exception e) {
+							Debug.log(Debug.ERROR, "Too many players in the map for local game");
+							return;
 						}
-						Game.staticBackground.add(new Background(x
-								* Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
-					} else if (type == 4) {
-						Game.entities.add(new Finishpoint(x * Game.BLOCK_SIZE,
-								y * Game.BLOCK_SIZE));
-						Game.staticBackground.add(new Background(x
-								* Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
-					} else if (type == 5) {
-						Game.entities.add(new WallWithFinishingPoint(x
-								* Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
-
 					}
+					Game.staticBackground.add(new Background(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
+				} else if (type == 4) {
+					Game.entities.add(new Finishpoint(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
+					Game.staticBackground.add(new Background(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
+				} else if (type == 5) {
+					Game.entities.add(new WallWithFinishingPoint(x * Game.BLOCK_SIZE, y * Game.BLOCK_SIZE));
 
 				}
-				y++;
+				x++;
 			}
-			Game.FIELD_HEIGHT = y;
-			Game.FIELD_WIDTH = x;
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			y++;
 		}
-
+		Game.FIELD_HEIGHT = y;
+		Game.FIELD_WIDTH = x;
 	}
 
 	/**
@@ -96,12 +115,12 @@ public class Loader {
 		int x = 0, type, y = 0;
 		int player_count = 0;
 
+		String content = this.loadMap(filename);
+
 		Scanner maps;
 		try {
 
-			maps = new Scanner(
-					Main.class.getResourceAsStream("/ressources/maps/"
-							+ filename));
+			maps = new Scanner(content);
 			while (maps.hasNextLine()) {
 				String text = maps.nextLine();
 				for (x = 0; x < text.length(); x++) {
@@ -125,12 +144,12 @@ public class Loader {
 		int x = 0, type, y = 0;
 		ArrayList<Point> spawnpoints = new ArrayList<Point>();
 
+		String content = this.loadMap(filename);
+
 		Scanner maps;
 		try {
 
-			maps = new Scanner(
-					Main.class.getResourceAsStream("/ressources/maps/"
-							+ filename));
+			maps = new Scanner(content);
 			while (maps.hasNextLine()) {
 				String text = maps.nextLine();
 				for (x = 0; x < text.length(); x++) {
@@ -148,5 +167,32 @@ public class Loader {
 		}
 
 		return spawnpoints;
+	}
+
+	/*
+	 * http://stackoverflow.com/questions/326390/how-to-create-a-java-string-from
+	 * -the-contents-of-a-file
+	 */
+	public static String readFile(String path) throws IOException {
+		FileInputStream stream = new FileInputStream(new File(path));
+		try {
+			FileChannel fc = stream.getChannel();
+			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+			/* Instead of using default, pass in a decoder. */
+			return Charset.defaultCharset().decode(bb).toString();
+		} finally {
+			stream.close();
+		}
+	}
+
+	public static String readFile(InputStream f) throws IOException {
+		Scanner s = new Scanner(f);
+		StringBuilder sb = new StringBuilder();
+		while (s.hasNextLine()) {
+			sb.append(s.nextLine());
+			sb.append("\r\n");
+		}
+		return sb.toString();
+
 	}
 }
